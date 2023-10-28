e8d9118f274f616fbe75a93e545a836a1f7aca12
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2023-10-28 17:03:08 +0800

    Refactor script, Add basic GUI
    Refactored Tic-Tac-Toe script to implement basic web page GUI,
    code organisation and readability.

--- script.js
+++ script.js
@@ -1,116 +1,81 @@
-//  Current Goal: To make a tic-tac-toe game against a naive computer.
-//  
-//      To implement:
-//          gameboard object:
-//              1. The gameboard layout
-//                  a. Layout stored as 3x3 2D array.
-//                  b. 3 Possible values to store: 'X' for Player 1, 'O' for Player 2, '' for empty.
-//              2. Setter & Getter methods
-//                  a. Getter -> return gameboard layout
-//                  b. Setter -> set a value within gameboard layout w/ validation logic
-//                          
-//        gameController object:
-//             1. tracks current player turn
-//             2. updates gameboard with player's play
-//             3. validates win/draw/lose conditions
 
-
-
-// gameboard object that controls the gameboard layout.
 const gameboard = (() => {
 
-    // Create board.
-    const board = [];
-    const dims = 3;
-        for (let i = 0; i < dims; i++) {
-            board.push([]);
-            for (let j = 0; j < dims; j++) {
-                board[i].push('');
-            }
-        }
+    const board = (function createBoard() {
+        const board = [];
+        for(let i = 0; i < 9; i++) board.push('');
+        return board;
+    })();
 
-    // Method - Get board.
+
     const getBoard = () => board;
-    
-    // Method - Mark position on board with given marker.
-    const placeMarker = (marker, row, column) => {
-        board[row][column] = marker;
-    }
 
-    // Method - Print current board onto console.
     const printBoard = () => {
-        board.map((row) => console.log(row));
+        console.log(board.slice(0,3));
+        console.log(board.slice(3,6));
+        console.log(board.slice(6,9));
     }
 
-    return {getBoard, placeMarker, printBoard}
+    const markCell = (marker, cell) => {
+        board[cell] = marker;
+    }
+    
+    const getLegalCells = () => {
+        const legalCells =  [...board.entries()].filter((pair) => (pair[1] === ''));
+        return legalCells.map((pair => pair[0]));
+    }
+
+    return {getBoard, printBoard, markCell, getLegalCells}
 })();
 
 const gameController = (() => {
 
     // Factory Function - Player
-    function Player(name, marker, type) {
+    const Player = (name, marker, type) => {
         return {name, marker, type};
     }
 
-    // Factory Function - Computer Player
-    const ComputerPlayer = (marker) => {
+    // Factory Function - Computer Player extends Player
+    const Computer = (marker) => {
         const {name, type} = Player('Computer', marker, 'computer');
-
-        const promptSelection = (board) => {
-            // Format board as flattened 1D array with remaining legal grids/move (i.e., grids that still have '') filtered in.
-            const filteredBoard = [];
-            const keyValueArr = [...board.flat().entries()].filter(pair => pair[1] === '');
-            keyValueArr.forEach((pair) => filteredBoard.push(pair[0]));
-
-            // Randomly select a legal grid from formatted board using index.
-            const idx = Math.round(Math.random() * (filteredBoard.length - 1));
-            const selection = filteredBoard[idx];
-
-            // Reformat legal grid to original 2D board indices.
-            const row = Math.floor(selection / 3);
-            const column = selection % 3;
-            
-            return [row, column];
+        
+        const randomSelection = () => {
+            const legalCellsIdxs = gameboard.getLegalCells();
+            const selection = legalCellsIdxs[Math.round(Math.random() * (legalCellsIdxs.length))];
+            console.log(legalCellsIdxs, selection);
+            return selection;
         }
-        return {name, marker, type, promptSelection}
+        return {name, marker, type, randomSelection}
     }
 
-    // Factory Function - Human Player
-    const HumanPlayer = (name, marker) => {
+    // Factory Function - Human Player extends Player
+    const Human = (name, marker) => {
         const {type} = Player(name, marker, 'human');
-
-        const promptSelection = () => {
-            selection = prompt("Select a grid (from 0 to 8)");
-
-            const row = Math.floor(selection / 3);
-            const column = selection % 3;
-            
-            return [row, column];
-        }
-        return {name, marker, type, promptSelection}
+        return {name, marker, type}
     }
 
-        const player1 = HumanPlayer('Tom', 'X');
-        const player2 = ComputerPlayer('O');
+    const player1 = Human('Tom', 'X');
+    const player2 = Computer('O');
 
-        let activePlayer = (Math.round(Math.random())) ? player1 : player2;
-        console.log(`${activePlayer.name} goes first!`);
+    let activePlayer = (Math.round(Math.random())) ? player1 : player2;
+    console.log(`${activePlayer.name} goes first!`); // debug
 
-        const board = gameboard.getBoard();
-        console.log(player2.promptSelection(board)) // debug
+    const board = gameboard.getBoard();
 
-    // Method - Plays the current player's turn, updates the board, and stops at the start of other player's turn.
-    const playTurn = () => {
-        gameboard.placeMarker(activePlayer.marker, ...activePlayer.promptSelection(board));
-        gameboard.printBoard();
+    const playTurn = (cell) => {
+        if (activePlayer.type === 'computer') gameboard.markCell(activePlayer.marker, activePlayer.randomSelection());
+        if (activePlayer.type === 'human') gameboard.markCell(activePlayer.marker, cell);
+        screenController.updateScreen();
+        gameboard.printBoard(); // debug
 
         const gameStatus = getGameStatus();
-        if(gameStatus === 'win') return activePlayer;
-        if(gameStatus === 'draw') return console.log("Draw!");
+        if(gameStatus === 'win') return console.log(`${activePlayer.name} wins!`); // debug
+        if(gameStatus === 'draw') return console.log("Draw!"); // debug
+
         switchActivePlayer();
+        if (activePlayer.type === 'computer') playTurn();
     };
 
-    // Method - Switch players.
     const switchActivePlayer = () => activePlayer = (activePlayer == player1) ? player2 : player1;
 
     // Method - Check for winning/draw conditions.
@@ -118,45 +83,35 @@ const gameController = (() => {
 
         const isRowWinner = (function validateRows() {
             for (let i = 0; i < 3; i++) {
-                const uniqueElements = board[i].filter((el, idx, row) => row.indexOf(el) === idx);
-                if (uniqueElements.length === 1 && !uniqueElements.includes('')) return true;
+                const arr = [board[0+(3*i)], board[1+(3*i)], board[2+(3*i)]];
+                if (!(arr.includes('')) && (arr.every((marker) => marker === board[0+3*i]))) return true;
             }
         })();
+
         if (isRowWinner) return 'win';
 
         const isColumnWinner = (function validateColumns() {
             for (let i = 0; i < 3; i++) {
-                let column = [];
-                for (let j = 0; j < 3; j++) {
-                    column.push(board[j][i]);
-                }
-                const uniqueElements = column.filter((el, idx, column) => column.indexOf(el) === idx);
-                if (uniqueElements.length === 1 && !uniqueElements.includes('')) return true;
+                const arr = [board[0+(1*i)], board[3+(1*i)], board[6+(1*i)]];
+                if (!(arr.includes('')) && (arr.every((marker) => marker === board[0+(1*i)]))) return true;
             }
         })();
+
         if (isColumnWinner) return 'win';
 
         const isDiagonalWinner = (function validateDiagonals() {
-            let leftDiagonal = [];
-            let rightDiagonal = [];
-            for (let i = 0; i < 3; i++) {
-                    leftDiagonal.push(board[i][i]);
-                    rightDiagonal.push(board[i][(board.length-1) - i]);
-                }
-            for (diagonal of [leftDiagonal, rightDiagonal]) {
-                const uniqueElements = diagonal.filter((el, idx, diagonal) => diagonal.indexOf(el) === idx);
-                if (uniqueElements.length === 1 && !uniqueElements.includes('')) return true;
-            }
+            const leftDiag = [board[0], board[4], board[8]];
+            const rightDiag = [board[2], board[4], board[6]];
+            if (!(leftDiag.includes('')) && (leftDiag.every((marker) => marker === board[0]))) return true;
+            if (!(rightDiag.includes('')) && (rightDiag.every((marker) => marker === board[2]))) return true;
         })();
+
         if (isDiagonalWinner) return 'win';
 
         const isDraw = (function validateDraw() {
-            flatten = [];
-            for (let i = 0; i < 3; i++) {
-                flatten = flatten.concat(board[i]);
-            }
-            if (!flatten.includes('')) return true;
+            if (!board.includes('')) return true;
         })();
+        
         if(isDraw) return 'draw';
 
         return 'in-play';
@@ -165,10 +120,35 @@ const gameController = (() => {
     return {playTurn};
 })();
 
-function test() {
-    const board = gameboard.getBoard();
-    const arr = [...board.flat().entries()].filter(pair => pair[1] === '');
-    let temp = [];
-    arr.forEach((pair) => temp.push(pair[0]));
-    return temp;
-}
+const screenController = (() => {
+
+    const gameboardDiv = document.querySelector('.gameboard');
+
+    // Create Tic-Tac-Toe HTML layout
+    for (let i = 0; i < 9; i++) {
+        const cellButton = document.createElement('button');
+        cellButton.setAttribute('class', 'cell');
+        cellButton.setAttribute('type', 'button');
+        cellButton.dataset.index = i;
+        gameboardDiv.appendChild(cellButton);
+    }
+
+    const updateScreen = () => {
+        const board = gameboard.getBoard();
+        const cellDivs = [...document.querySelectorAll('.cell')];
+        const boardMarkers = board.flat();
+
+        cellDivs.forEach((cellDiv, i) => cellDiv.textContent = boardMarkers[i]);
+    };
+
+    const gameboardClickHandler = (e) => {
+        const Cell = e.target.dataset.index;
+        if (!Cell) return;
+
+        gameController.playTurn(Cell);
+    }
+
+    gameboardDiv.addEventListener('click', gameboardClickHandler);
+
+    return {updateScreen};
+})();
